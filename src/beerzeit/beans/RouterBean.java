b7ac27eced4ef522ac90cf1cd3a89b1c5904b7a3
Pedro Nakibar <pedronakibar@gmail.com>
2016-11-28 11:35:41 -0200

    navbar programatically chaing the active tab

---
+++ src/beerzeit/beans/RouterBean.java
@@ -0,0 +1,27 @@
+package beerzeit.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ * Created by pedro on 28/11/16.
+ */
+@ManagedBean(name = "routerbean")
+@SessionScoped
+public class RouterBean  {
+    public String getViewId() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        String viewId = facesContext.getViewRoot().getViewId();
+        return viewId;
+    }
+    public String getNavClass(String current) {
+        String r = getViewId().contentEquals(current) ?
+                        "active" :
+                        "";
+        System.out.println(current);
+        System.out.println(getViewId());
+        System.out.println(r);
+        return r;
+    }
+}
